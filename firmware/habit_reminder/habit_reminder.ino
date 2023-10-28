3da7ff6a101f1dd2cbad9b84933a2ddfde2c9ed4
Rikeroo <rkwllsn@gmail.com>
2023-10-28 14:57:44 +0100

    OOP Implementation

--- firmware/habit_reminder/habit_reminder.ino
+++ firmware/habit_reminder/habit_reminder.ino
@@ -10,17 +10,30 @@ int neoPin = 5;
 int numPixels = 3;
 
 int boxLength = 103; // Menu box length
-int boxWidth = 20; // Meny box width
+int boxWidth = 20; // Menu box width
+int symbolX = 109; // Symbol X draw coord
+int symbolY = 16; // Initial symbol Y
 
 int button1Pin = 35; // Left most button pin
 int duration = 2000; // Long Press Duration
 
+// Menu naviagtion parameters
+int menuOption = 0;
+
+// Define task class
+class Task {
+  public:
+    std::string text;
+    bool complete;
+};
+
+// Define Tasks
+Task task1 = {"Take Multi-Vitamins", false};
+Task task2 = {"Do Physio Exercises", false};
+Task task3 = {"Check Calendar", false};
+
 // Define buttons
 EasyButton button1(button1Pin);
-
-void tickBox() {
-  
-}
 
 Adafruit_NeoPixel pixels(numPixels, neoPin, NEO_GRB + NEO_KHZ800);
 
@@ -28,10 +41,25 @@ void buttonPress() {
   Serial.println("Button pressed");
   pixels.setPixelColor(1, pixels.Color(0,150,0));
   pixels.show();
+  
+  // Increment option and reset at end
+  if (menuOption < 2) {
+    menuOption = menuOption + 1;
+  } else {
+    menuOption = 0;
+  }
+  // Set Symbol Y based on menu option
+  symbolY = 16 + (menuOption*22);
+  
+  Serial.println(menuOption);
 }
 
 void onPressedForDuration() {
   Serial.println("Button Long Press");
+
+  // Draw tick at selected option
+  u8g2.setFont(u8g2_font_unifont_t_symbols);
+  u8g2.drawGlyph(symbolX,symbolY, 0x2714); //Draw tick
 }
 
 void setup() {
@@ -57,18 +85,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //delay(10); // this speeds up the simulation
-
   u8g2.firstPage();
   do {
+    //u8g2.setFont(u8g2_font_squeezed_b7_tr);
     u8g2.setFont(u8g2_font_squeezed_r7_tr);
 
     // Draws Tasks Text
-    u8g2.drawStr(3,17,"Take Multi-vitamins");
-
-    u8g2.drawStr(3,39,"Do Physio Exercises");
-
-    u8g2.drawStr(3,61,"Check Calendar");
+    u8g2.drawStr(3,17,(task1.text).data());
+    u8g2.drawStr(3,39,(task2.text).data());
+    u8g2.drawStr(3,61,(task3.text).data());
 
     // Draws Menu Boxes and check boxes
     for (int i = 0; i < 3; i++) {
@@ -78,12 +103,18 @@ void loop() {
       // Define cross draw coordinates
       int crossX = 109;
       int crossY = 16+(i*22);
-      
-      // Draw crosses in boxes
-      u8g2.setFont(u8g2_font_unifont_t_symbols);
-      u8g2.drawGlyph(crossX,crossY, 0x2716);
+
+      // Draw only first time
+      for (int j = 0; j<1; j++) {
+        // Draw crosses in boxes
+        u8g2.setFont(u8g2_font_unifont_t_symbols);
+        u8g2.drawGlyph(crossX,crossY, 0x2715);
+      }
       
     }
+    // Menu navigation
+    
+    
   } while ( u8g2.nextPage() );
 
   // Poll buttons
